57e4a4f7d356afe9a50a7dee351e4afe6aa6e40d
popipopka <eg.belykh@yandex.ru>
2025-04-16 19:43:42 +0300

    feat: add generation activity
    VM-72

---
+++ app/src/main/java/app/visualmusic/presentation/generation/GenerationActivity.kt
@@ -0,0 +1,26 @@
+package app.visualmusic.presentation.generation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowInsetsCompat.Type.statusBars
+import app.visualmusic.common.ui.edgeToEdge
+import app.visualmusic.common.ui.paddingBy
+import app.visualmusic.databinding.ActivityGenerationBinding
+
+class GenerationActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityGenerationBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityGenerationBinding.inflate(layoutInflater)
+        setupEdgeToEdge()
+        setContentView(binding.root)
+    }
+
+    private fun setupEdgeToEdge() {
+        edgeToEdge {
+            binding.root paddingBy statusBars()
+        }
+    }
+}
