2262e8d74b8b6ec070fb0fd989ef26c614a2fda7
popipopka <eg.belykh@yandex.ru>
2025-04-17 18:32:02 +0300

    feat: add auto generation screen
    VM-69

---
+++ app/src/main/java/app/visualmusic/presentation/generation/screen/AutoGenerationFragment.kt
@@ -0,0 +1,25 @@
+package app.visualmusic.presentation.generation.screen
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import app.visualmusic.databinding.FragmentAutoGenerationBinding
+
+class AutoGenerationFragment : Fragment() {
+    private lateinit var binding: FragmentAutoGenerationBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentAutoGenerationBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
